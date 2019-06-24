edf70e76ff56952fe2debfca97e5588496632265
elieser pereira <elieser.pereiraa@gmail.com>
2019-06-24 17:50:07 -0300

    update projec dirs

---
+++ python/setup.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+from setuptools import setup
+
+setup(name=' paquete',
+      version='1.0.99',
+      packages=['common'],
+      data_files=[('common/visualization/templates', ['testblahblabpakcage.html',
+                                                      'testblahblabpakcageion.html',
+                                                      'testblahblabpakcageplication.html',
+                                                      'testblahblabpakcageds.html',
+                                                      'testblahblabpakcagenjugation_models.html',
+                                                      'testblahblabpakcageresume.html'])],
+      include_package_data=True,
+      zip_safe=False)
