b924dab776b42db6120bcc430c0fffa953478a8b
elieser pereira <elieser.pereiraa@gmail.com>
2019-06-24 18:23:06 -0300

    update version

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name=' paquete',
-      version='1.0.99',
+      version='1.0.100',
       packages=['common'],
       data_files=[('common/visualization/templates', ['testblahblabpakcage.html',
                                                       'testblahblabpakcageion.html',
